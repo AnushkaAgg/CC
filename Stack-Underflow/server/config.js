be92254b748f5bd88ddd30dc2e464c7fd9d09595
AnushkaAg <54896339+AnushkaAgg@users.noreply.github.com>
2023-05-29 21:29:22 +0530

    Add files via upload

---
+++ Stack-Underflow/server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    MONGODB:"mongodb+srv://CC:majorproject@campusconnect.u967uxf.mongodb.net/?retryWrites=true&w=majority",
+    SECRET_KEY : 'NOTESAPI',
+    EMAIL_ID : 'anushka.agarwal1102@gmail.com',
+    PASSWORD : 'Aa@12345',
+    CLIENT_ID : '610469225992-nj08gvg39f2ubgd8uggfef9hcu9i9tgf.apps.googleusercontent.com',
+    CLIENT_SECRET : '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',
+};
